9afcf853f2e5cca6d81f248476aa655cdd7f643d
Bking625 <ddiddikk@naver.com>
2019-05-30 09:42:18 +0900

    springex4 (web.xml XXX)

---
+++ spring-study-springex4/src/main/java/com/cafe24/springex/initializer/SpringexWebApplicationInitializer.java
@@ -0,0 +1,28 @@
+package com.cafe24.springex.initializer;
+
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.cafe24.springex.config.WebConfig;
+
+public class SpringexWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		//AppConfig
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		//WebConfig
+		return new Class<?>[] {WebConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		//dispetcherserv
+		return new String[] {"/"};
+	}
+
+}
